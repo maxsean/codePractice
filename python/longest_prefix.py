345d4d6d396ad24e126ec9f6c784dc94462bb9a5
maxsean <xsma1990@gmail.com>
2018-01-21 16:58:56 -0500

    added find longest prefix algorithm

---
+++ python/longest_prefix.py
@@ -0,0 +1,13 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        for i, letter_group in enumerate(zip(*strs)):
+            if len(set(letter_group)) > 1:
+                return strs[0][:i]
+        else:
+            return min(strs)
+        
